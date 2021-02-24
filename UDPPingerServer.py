d4ca8233669081718130f2cdfcb04b96812b525b
Gagan Bhatia <gagan3012@users.noreply.github.com>
2021-02-24 22:37:54 +0300

    Update UDPPingerServer.py

--- UDPPingerServer.py
+++ UDPPingerServer.py
@@ -12,13 +12,12 @@ serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', 12000))
 
 while True:
-    # Generate random number in the range of 0 to 10
+    # Generate random number in the range of 0 to 49
     rand = random.randint(0, 49)
 
     # Receive the client packet along with the address it is coming from
     message, address = serverSocket.recvfrom(1024)
-
-    # Capitalize the message from the cliente
+    # Capitalize the message from the client
     message = message.upper()
 
     # If rand is less is than 18, we consider the packet lost and do not respond
@@ -27,3 +26,4 @@ while True:
 
     # Otherwise, the server responds
     serverSocket.sendto(message, address)
+    
